a4e0421496f0299584491a76d0495658849f7090
Harpreet Singh <hpmehra@ymail.com>
2017-01-09 22:50:30 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace XSS_with_Ajax_Requests.Models
+{
+    public class Employee
+    {
+        
+        public int ID { get; set; }
+
+        
+        public string Name { get; set; }
+
+       
+        public float Salary { get; set; }
+    }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XSS_with_Ajax_Requests.Models;
+
+
+
+namespace XSS_with_Ajax_Requests.Controllers
+{
+    [FilterConfig.ValidateAntiForgeryTokenOnAllPosts]
+    public class EmployeeController : Controller
+    {
+
+        public class UsersContext : DbContext
+        {
+            public UsersContext()
+                : base("DefaultConnection")
+            {
+            }
+
+            public DbSet<Employee> Employee { get; set; }
+        }
+
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+
+        public ActionResult SaveData(string name, float salary)
+        {
+            UsersContext usrContext = new UsersContext();
+            Employee emp = new Employee();
+            emp.Name = name;
+            emp.Salary = salary;
+            usrContext.Employee.Add(emp);
+            usrContext.SaveChanges();
+            return Json("Successfull", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
